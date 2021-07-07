b5d6673e67adc3330dbaeb3aa21aab48eb5a014d
Ryan Breuer <breuerryan@gmail.com>
2021-07-07 00:35:32 -0500

    Cache character geometries for performance

--- src/js/components/CharacterObject.js
+++ src/js/components/CharacterObject.js
@@ -2,6 +2,7 @@ import _PositionalObject from "./_PositionalObject";
 import * as THREE from "three";
 import helvetikerFont from "../../fonts/helvetiker_regular.typeface.json";
 
+const cachedTextGeometries = [];
 export default class CharacterObject extends _PositionalObject {
     constructor(args = {}) {
         if (args.components && args.components.characterobject) {
@@ -24,17 +25,30 @@ export default class CharacterObject extends _PositionalObject {
 
     createObject() {
         const font = new THREE.Font(this.font);
-        this.geometry = new THREE.TextGeometry(this.letter, {
-            font: font,
-            size: 4.1/5 * this.depth,
-            height: this.scale * this.depth,
-            curveSegments: 24,
-            bevelEnabled: true,
-            bevelThickness: .05,
-            bevelSize: .05,
-            bevelOffset: 0,
-            bevelSegments: 1
-        });
+        let anyFound = false;
+        for (const geometry of cachedTextGeometries) {
+            if (geometry.parameters.options.font.data.familyName === font.data.familyName && geometry.parameters.options.height === this.scale * this.depth && geometry.letter === this.letter) {
+                this.geometry = geometry;
+                anyFound = true;
+                break;
+            }
+        }
+
+        if (!anyFound) {
+            this.geometry = new THREE.TextGeometry(this.letter, {
+                font: font,
+                size: 4.1 / 5 * this.depth,
+                height: this.scale * this.depth,
+                curveSegments: 24,
+                bevelEnabled: true,
+                bevelThickness: .05,
+                bevelSize: .05,
+                bevelOffset: 0,
+                bevelSegments: 1
+            });
+            this.geometry.letter = this.letter;
+            cachedTextGeometries.push(this.geometry);
+        }
 
         this.object = new THREE.Mesh(
             this.geometry,
