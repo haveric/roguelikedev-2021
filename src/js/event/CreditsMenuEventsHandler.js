ffcff71d0fc410af07021460696319272046af6f
Ryan Breuer <breuerryan@gmail.com>
2021-08-01 21:15:18 -0500

    Autoclose credits after animation

--- src/js/event/CreditsMenuEventsHandler.js
+++ src/js/event/CreditsMenuEventsHandler.js
@@ -4,13 +4,18 @@ import MenuEventHandler from "./_MenuEventHandler";
 export default class CreditsMenuEventsHandler extends MenuEventHandler {
     constructor(eventHandler) {
         super(eventHandler);
+        const self = this;
 
         creditsMenu.open();
+        this.timeout = setTimeout(function() {
+            self.onLeftClick();
+        }, 31000);
     }
 
     teardown() {
         super.teardown();
 
+        clearTimeout(this.timeout);
         creditsMenu.close();
     }
 
